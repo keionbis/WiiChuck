50d4e42d106095d45080bc98ed4cf35b9a551169
keionbis <keion96@gmail.com>
2018-02-10 14:04:12 -0500

    Filling array with values

--- examples/Arduino/Wireless_Controller_Classic/Wireless_Controller_Classic.ino
+++ examples/Arduino/Wireless_Controller_Classic/Wireless_Controller_Classic.ino
@@ -2,12 +2,17 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+Classic classic;
+int transmission_Array[18] = {0};
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  classic.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+   transmission_Array = {classic.getJoyYLeft(),classic.getJoyXLeft(),classic.getJoyYRight(),classic.getJoyXRight(), 
+                         classic.getTriggerLeft, classic.getTriggerRight, classic.getPadRight, classic.getPadDown,
+                         classic.getPadUp, classic.getPadLeft, classic.getButtonX, classic.getButtonY, classic.getButtonA,
+                         classic.getButtonB, classic.getButtonMinus, classic.getButtonPlus, classic.getButtonZLeft,
+                         classic.getButtonZRight};
 }
